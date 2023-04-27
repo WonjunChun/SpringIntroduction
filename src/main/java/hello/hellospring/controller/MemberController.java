d4624976fda05da24b709fb543f412cf62603ca1
chunjh1103 <chunjh1103@naver.com>
2023-04-27 11:05:12 +0900

    add: java config를 통한 Bean 등록 방식

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -7,7 +7,16 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-
+    /**
+     * 의존성 주입 - 생성자 주입 방식
+     *
+     * * 필드 주입 방식은 *
+     * @Autowired
+     * private MemberService memberService;
+     * 이런식으로
+     *
+     * * setter 주입 방식도 있음 *
+     */
     private final MemberService memberService;
 
     @Autowired
